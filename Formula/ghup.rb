a5964da0e2fb2bc8cdaebc6a23b1cdbab187915f
Robin Breathe <robin@isometry.net>
2026-08-10 17:48:35 +0200

    Update ghup to 0.21.0

--- Formula/ghup.rb
+++ Formula/ghup.rb
@@ -1,15 +1,15 @@
 class Ghup < Formula
   desc "A GitHub API client for managing tags and repository content from third-party automation systems"
   homepage "https://just.breathe.io/project/ghup/"
-  url "https://github.com/nexthink-oss/ghup/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "8bf87f36841848e34c6a15873946970256641f7723709948101876ed7e89ffe1"
+  url "https://github.com/nexthink-oss/ghup/archive/refs/tags/v0.21.0.tar.gz"
+  sha256 "cb112a0ac8c42d30183b11a3db16c621c05d4f4d558b36761cd512d6420cc532"
 
   bottle do
     root_url "https://ghcr.io/v2/isometry/tap"
-    sha256 cellar: :any_skip_relocation, monterey: "5c44e86ca9d2e5fbd99a7053cd3a69865b151762e1592a5780a5a698ec7431f1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "411ef7fba48bcfb9aea5fd294c1f1c8064ee6cef5fc42c6df989b38b4876f829"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "cbfc0e184f85a5b835a9833a78a56a755053c5dddb5c4ddd1ef32bd8b4fe48a4"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "c122c75f62e2514aa597ae113cfcda8b2525dfa1ed687b370ed1a24729547027"
+    sha256 cellar: :any_skip_relocation, monterey: "7f9da95d656ec34104b740cee5c45bbc6822b520b4ba7797a809670ff73ece07"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "029e0ddb14cc01f05a3a9c4fbfd4f80059a0635e5af98ef971ec0b3796e5d019"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d24d26e255d9d0a3f4f7ae78484914447737dc00e6a6588cfdb7320e3c4736cc"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "f1bd2ec347d75d74a54b3de0095c5e7c59bfa122bc6c08faefe1c9edfe3a1085"
   end
 
   def install
